4ace2334f20207a2abc48c0ddd2f4489e700227a
Vlad Drozdov <drozdov.vl.1@yandex.ru>
2023-09-17 13:48:20 +0400

    ПР 14 fix after review

--- app.js
+++ app.js
@@ -7,54 +7,27 @@ const { auth } = require('./middlewares/auth');
 const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const { NotFoundError } = require('./errors/errors');
-const urlPattern = new RegExp(
-  "^((http|https):\\/\\/)?(www\\.)?[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,6})+[a-zA-Z0-9-._~:\\/?#\\[\\]@!$&'()*+,;=]*$"
-);
 
-const emailPattern = new RegExp(
-  "^[^\s@]+@[^\s@]+\.[^\s@]+$"
-);
-
-const {
-  createUser,
-  login
-} = require('./controllers/users');
-
-const {
-  PORT = 3000,
-  DB_URL = 'mongodb://127.0.0.1:27017/mestodb',
-} = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
 
 const app = express();
+
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(express.json());
 
+const { signupValidation, signinValidation } = require('./middlewares/validation');
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}),login);
+app.post('/signin', signinValidation, login);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlPattern),
-  }),
-}),createUser);
+app.post('/signup', signupValidation, createUser);
 
 app.use(auth);
-
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
@@ -62,20 +35,11 @@ app.use(errors());
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
-})
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message
-    });
 });
 
-app.listen(PORT, () => {
+const errorHandler = require('./middlewares/error-handler');
+app.use(errorHandler);
 
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
